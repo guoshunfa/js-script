fdecdab4a252e45c96ef660cbf3c4bb3b5ca35ab
guoshunfa <me@guoshunfa.com>
2025-06-23 21:51:56 +0800

    调整 deploy-gh-pages.js ，兼容更多场景

--- deploy-gh-pages.js
+++ deploy-gh-pages.js
@@ -8,8 +8,11 @@
  * 4. 修改distDir为你的构建输出目录（默认为dist）
  * 5. 运行脚本
  * 6. 使用bun或者nodejs直接执行当前js脚本，bun deploy-gh-pages.js
+ * 
+ * 执行完后等待片刻，访问 https://<user_name>.github.io/<address>/ 即可看到部署的内容
  */
 import { execSync } from 'child_process';
+import fs from 'fs';
 
 const repoUrl = 'git@github.com:<user_name>/<address>.git';
 const branchName = 'gh-pages';
@@ -30,11 +33,25 @@ runCommand(buildCommand);
 
 process.chdir(distDir);
 
-runCommand('git init');
-runCommand(`git remote add origin ${repoUrl}`);
-runCommand(`git checkout -b ${branchName}`);
-runCommand('git add .');
-runCommand('git commit -m "Deploy dist folder to GitHub Pages"');
-runCommand(`git push -u origin ${branchName}`);
+// .git目录不存在时初始化git仓库
+if (!fs.existsSync(".git")) {
+    runCommand('git init');
+    runCommand(`git remote add origin ${repoUrl}`);
+    runCommand(`git checkout -b ${branchName}`);
+}
 
-console.log('successfully deployed to GitHub Pages');
+try {
+    // 检查是否有更改
+    const statusOutput = execSync('git status --porcelain').toString().trim();
+    if (statusOutput) {
+        runCommand('git add .');
+        runCommand('git commit -m "Deploy dist folder to GitHub Pages"');
+        runCommand(`git push -u origin ${branchName} --force`);
+        console.log('Successfully deployed to GitHub Pages.');
+    } else {
+        console.log('No changes detected in the dist folder. Deployment skipped.');
+    }
+} catch (error) {
+    console.error('Error checking git status:', error);
+    process.exit(1);
+}
